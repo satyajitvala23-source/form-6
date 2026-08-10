53b3afcd3cf0eea6b18e470e6554f2aa09cf495b
Shiv goswami <shivambharthi2000@gmail.com>
2026-08-10 23:54:13 +0530

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nm1 = new System.Windows.Forms.TextBox();
+            this.txt_nm2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_fin = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Schneidler Blk BT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First Num :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Schneidler Blk BT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Sec Num :";
+            // 
+            // txt_nm1
+            // 
+            this.txt_nm1.Location = new System.Drawing.Point(198, 103);
+            this.txt_nm1.Name = "txt_nm1";
+            this.txt_nm1.Size = new System.Drawing.Size(100, 22);
+            this.txt_nm1.TabIndex = 2;
+            // 
+            // txt_nm2
+            // 
+            this.txt_nm2.Location = new System.Drawing.Point(197, 156);
+            this.txt_nm2.Name = "txt_nm2";
+            this.txt_nm2.Size = new System.Drawing.Size(100, 22);
+            this.txt_nm2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Ivory;
+            this.button1.Font = new System.Drawing.Font("Schneidler Blk BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(388, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Add";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Ivory;
+            this.button2.Font = new System.Drawing.Font("Schneidler Blk BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(390, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 39);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sub";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Ivory;
+            this.button3.Font = new System.Drawing.Font("Schneidler Blk BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(390, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Mul";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Ivory;
+            this.button4.Font = new System.Drawing.Font("Schneidler Blk BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(390, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Div";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Ivory;
+            this.button5.Font = new System.Drawing.Font("Schneidler Blk BT", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(388, 290);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 38);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Clear";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Schneidler Blk BT", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(104, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(374, 50);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Simple Calculator";
+            // 
+            // lbl_fin
+            // 
+            this.lbl_fin.AutoSize = true;
+            this.lbl_fin.Font = new System.Drawing.Font("Schneidler Blk BT", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_fin.Location = new System.Drawing.Point(177, 371);
+            this.lbl_fin.Name = "lbl_fin";
+            this.lbl_fin.Size = new System.Drawing.Size(0, 19);
+            this.lbl_fin.TabIndex = 10;
+            this.lbl_fin.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleGreen;
+            this.ClientSize = new System.Drawing.Size(598, 450);
+            this.Controls.Add(this.lbl_fin);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_nm2);
+            this.Controls.Add(this.txt_nm1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nm1;
+        private System.Windows.Forms.TextBox txt_nm2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_fin;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1, num2, tot;
+
+            num1 = Convert.ToInt16(txt_nm1.Text);
+            num2 = Convert.ToInt16(txt_nm2.Text);
+            tot = num1 + num2;
+
+            lbl_fin.Text = "Your Addition Is :" + tot.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1, num2, tot;
+
+            num1 = Convert.ToInt16(txt_nm1.Text);
+            num2 = Convert.ToInt16(txt_nm2.Text);
+            tot = num1 - num2;
+
+            lbl_fin.Text = "Your Substraction Is :" + tot.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1, num2, tot;
+
+            num1 = Convert.ToInt16(txt_nm1.Text);
+            num2 = Convert.ToInt16(txt_nm2.Text);
+            tot = num1 * num2;
+
+            lbl_fin.Text = "Your Multiplication Is :" + tot.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1, num2, tot;
+
+            num1 = Convert.ToInt16(txt_nm1.Text);
+            num2 = Convert.ToInt16(txt_nm2.Text);
+            tot = num1 / num2;
+
+            lbl_fin.Text = "Your Division Is :" + tot.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            txt_nm1.Text = "";
+            txt_nm2.Text = "";
+            lbl_fin.Text = "";
+        }
+    }
+}
